9b34cbad28569da3686e4c1a1901b38b2bfdceaf
TheVinz <vincenzo.santomarco@gmail.com>
2018-04-06 22:57:14 +0200

    riorganizzato in cartelle decentemente

---
+++ PrototipoClassi-Vinz/ModelViewController/MVC/PlayerController.java
@@ -0,0 +1,46 @@
+//Sul client
+
+public class PlayerController {
+	private GameController gameController;
+	private Player player;
+	public PlayerController(Player player){
+		this.player=player;
+	}
+	public bindGameController(GameController gc){
+		this.GameController=gc;
+	}
+	public String getWindowFrameChoises(){
+		return player.getWindowFrameChoises();
+	}
+	public void setFrame(int choice){
+		player.setFrame(int choice);
+	}
+	public String getPlayer(){
+		return player.toString();
+	}
+	public String printDraftPool(){
+		return gameController.printDraftPool();
+	}
+
+	public String printToolCards(){
+		return gameController.printToolCards();
+	}
+
+	public void moveDice(int dice, int i, int j,Player player) throws InvalidMoveException{
+		try{
+			gameController.moveDice(dice,i,j,player);
+		}
+		catch(InvalidMoveException e){
+			throw e;
+		}
+	}
+
+	public void useToolCard(int card, View playerView) throws InvalidMoveException{
+		try {
+			gameController.useToolCard(card, playerView);
+		}
+		catch(InvalidMoveException e){
+			throw e;
+		}
+	}
+}
