5a42b366095583cb453df295c50cc180492e2d41
gabristuc <37112750+gabristuc@users.noreply.github.com>
2018-04-07 11:41:31 +0200

    Update PrivateObjectiveCard.java

--- PrototipoClassi-Vinz/ObjectiveCards/PrivateObjectiveCards/PrivateObjectiveCard.java
+++ PrototipoClassi-Vinz/ObjectiveCards/PrivateObjectiveCards/PrivateObjectiveCard.java
@@ -3,5 +3,5 @@ package ObjectiveCards.PrivateObjectiveCards;
 public class PrivateObjectiveCard implements ObjectiveCard{
 	private IPrivateObjective privateObjective;
 	@Override
-	public int calcolaPunti(Vetrata vetrata);
+	public int calculatePoints(WindowFrame windowFrame);
 }
