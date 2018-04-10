13b4ebbb256d111def90a085ba0dc89ac20f53f0
dev-strenuus <andrea5.scotti@mail.polimi.it>
2018-04-10 21:03:34 +0000

    prova

--- Sagrada/src/main/java/ToolCards/IAbility.java
+++ Sagrada/src/main/java/ToolCards/IAbility.java
@@ -1,9 +1,7 @@
-package ToolCaards;
+package ToolCards;
 
-public interface IAbility{ù
-	//Ho inserito il parametro playerView in modo tale che la tool card possa richiamare il metodo print della view per stampare a schermo 
-	//la richiesta dei parametri e richiamare il metodo getInput in modo da avere input da tastiera da parte della view
-	//Il metodo getInput della view è scritto in modo tale che la tool card richieda parametri di tipo int.
-	//Il riferimento al model serve per poter trasformare l' input del giocatore in oggetti.
-	public void doAbility(Model model, View playerView) throws MossaNonValidaException;
+public interface IAbility{
+	int getNumberOfParameters();
+	void setParameter(Object o);
+	void doAbility() throws MossaNonValidaException;
 }
