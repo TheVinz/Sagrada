84a82101ff45ec5521ce8c60432570f8b08517c6
dev-strenuus <andrea5.scotti@mail.polimi.it>
2018-04-24 14:37:55 +0000

    test day

--- Sagrada/src/main/java/server/state/boards/draftpool/DraftPoolCell.java
+++ Sagrada/src/main/java/server/state/boards/draftpool/DraftPoolCell.java
@@ -4,36 +4,6 @@ import server.state.dice.Dice;
 import server.state.boards.Cell;
 import common.exceptions.InvalidMoveException;
 
-public class DraftPoolCell implements Cell{
-	private Dice dice;
+public class DraftPoolCell extends Cell{
 
-	public Dice getDice(){
-		return this.dice;
-	}
-
-	public void move(Cell target) throws InvalidMoveException{
-		if(this.isEmpty()) throw new InvalidMoveException("Empty cell");
-		else {
-			target.put(dice);
-			dice = null;
-		}
-	}
-
-	public void put(Dice dice) throws InvalidMoveException{
-		if(!this.isEmpty()) throw new InvalidMoveException("Already filled cell");
-		else{
-			this.dice=dice;
-		}
-	}
-
-	public boolean isEmpty(){
-		return dice==null;
-	}
-
-	@Override
-	public Dice removeDice() {
-		Dice result=this.dice;
-		this.dice=null;
-		return result;
-	}
 }
