13252bd3e6b3430b4c1a34d60bab418f4f198751
gabristuc <gabriele1.stucchi@mail.polimi.it>
2018-05-23 10:13:02 +0200

    test

--- Sagrada/src/main/java/client/view/cli/CliApp.java
+++ Sagrada/src/main/java/client/view/cli/CliApp.java
@@ -10,6 +10,28 @@ import client.view.cli.cliphasestate.*;
 import common.RemoteMVC.RemoteController;
 import common.command.GameCommand;
 
+/*
+Se dopo aver tolto un dado da una cella della draftpool richiedo quella cella mi ritorna questo, direi di bloccarlo
+prima di chiedere la coordinata della windowframe ( saremmo nello stato SelectingWindowFrameCell
+Select the number of the cell: 0
+Select the first coordinate of the WindowFrame: null
+*/
+/*
+Alla fine del round c'è da aggiornare la RoundTrack, non si rempie
+ */
+/*
+se per caso schiaccio S per vedere lo stato di un altro giocatore resto bloccato se non so il nome, direi di dover
+stampare i nomi degli altri e magari anche la possibilità di tornare indietro
+ */
+/*
+Ho mosso un dado dove non potevo e me l'ha fatto mettere, non bene..o non abbiamo ancora messo il controllo sulla Cli?
+Inoltre perchè mi stampa questa frase nel farlo?
+You can't insert anything now
+ */
+/*
+tool card sembra come non entri nello stato, me le stampa ma non mi chiede di sceglierne una
+ */
+
 public class CliApp {
 
     private RemoteController remoteController;
