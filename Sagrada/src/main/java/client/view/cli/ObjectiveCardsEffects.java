f4f0d434dbb0161678057aeca8c32d1709564d34
gabristuc <gabriele1.stucchi@mail.polimi.it>
2018-05-24 10:47:36 +0200

    cli update

--- Sagrada/src/main/java/client/view/cli/ObjectiveCardsEffects.java
+++ Sagrada/src/main/java/client/view/cli/ObjectiveCardsEffects.java
@@ -4,25 +4,25 @@ public class ObjectiveCardsEffects {
     public String returnEffects(int ObjectiveCard){
         switch (ObjectiveCard){
             case 0:
-                return "COLORED DIAGONAL:\nNumber of dices with same color in adjacent diagonal.\n\n";
+                return "COLORED DIAGONAL:\n  Number of dices with same color in adjacent diagonal.\n\n";
             case 1:
-                return "DARK SHADES:\n2 point for each set of 5 and 6.\n\n";
+                return "DARK SHADES:\n  2 points for each set of 5 and 6.\n\n";
             case 2:
-                return "DIFFERENT COLORS:\n4 points for each set of different colored dices.\n\n";
+                return "DIFFERENT COLORS:\n  4 points for each set of different colored dices.\n\n";
             case 3:
-                return "DIFFERENT COLORS COLUMN:\n5 points for each column without repeated colors.\n\n";
+                return "DIFFERENT COLORS COLUMN:\n  5 points for each column without repeated colors.\n\n";
             case 4:
-                return "DIFFERENT COLORS ROW:\n6 points for each row without repeated colors.\n\n";
+                return "DIFFERENT COLORS ROW:\n  6 points for each row without repeated colors.\n\n";
             case 5:
-                return "DIFFERENT SHADES:\n5 points for each set of different shade dices.\n\n";
+                return "DIFFERENT SHADES:\n  5 points for each set of different shade dices.\n\n";
             case 6:
-                return "DIFFERENT SHADES COLUMN:\n4 points for each column without repeated shade.\n\n";
+                return "DIFFERENT SHADES COLUMN:\n  4 points for each column without repeated shade.\n\n";
             case 7:
-                return "DIFFERENT SHADES ROW:\n5 points for each row without repeated shade.\n\n";
+                return "DIFFERENT SHADES ROW:\n  5 points for each row without repeated shade.\n\n";
             case 8:
-                return "MEDIUM SHADES:\n2 points for each set of 3 and 4.\n\n";
+                return "MEDIUM SHADES:\n  2 points for each set of 3 and 4.\n\n";
             case 9:
-                return "PALE SHADES:\n2 point for each set of 1 and 2.\n\n";
+                return "PALE SHADES:\n  2 points for each set of 1 and 2.\n\n";
             default:
                 return "error";
         }
