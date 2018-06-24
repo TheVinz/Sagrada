9ca2caa1ef44b708240d7d4858acdcbc00126cda
dev-strenuus <andrea5.scotti@mail.polimi.it>
2018-06-24 22:21:26 +0000

    starting javadoc in viewchangement

--- src/main/java/common/viewchangement/Changement.java
+++ src/main/java/common/viewchangement/Changement.java
@@ -4,7 +4,15 @@ import common.Changer;
 
 import java.io.Serializable;
 
+/**
+ * The <tt>Changement</tt> class contains data to send to the client to inform him about a change in the state.
+ */
 public abstract class Changement implements Serializable {
 
+
+    /**
+     * Delegate the handling of this <tt>Changement</tt> to a specific {@link common.Changer}.
+     * @param changer will handle this <tt>Changement</tt>.
+     */
     public abstract void change(Changer changer);
 }
